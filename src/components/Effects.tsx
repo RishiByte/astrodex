19d3f015543ff5c49931a6df845fd10f92dffcc1
Rishi Bhardwaj <rishibhardwaj1210@gmail.com>
2026-07-24 23:43:36 +0530

    refactor: Refactor the Post-processing Bloom (fixes #528)

--- src/components/Effects.tsx
+++ src/components/Effects.tsx
@@ -3,15 +3,17 @@
 import { EffectComposer, Bloom, Vignette } from "@react-three/postprocessing"
 import { BlendFunction } from "postprocessing"
 
+const BLOOM_CONFIG = {
+  intensity: 1.5,
+  luminanceThreshold: 0.6,
+  luminanceSmoothing: 0.02,
+  mipmapBlur: true,
+}
+
 export function Effects() {
   return (
     <EffectComposer>
-      <Bloom
-        intensity={1.5}
-        luminanceThreshold={0.6}
-        luminanceSmoothing={0.02}
-        mipmapBlur
-      />
+      <Bloom {...BLOOM_CONFIG} />
       <Vignette
         offset={0.3}
         darkness={0.7}
